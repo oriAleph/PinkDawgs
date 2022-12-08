6a61184fad2d9d7ad1eda5cd0a48c6fe4a89d85e
Oriana <orianajb18@outlook.com>
2022-12-08 00:52:42 -0600

    added cocoapods

---
+++ LEGO Brick Finder/Pods/Target Support Files/Pods-LEGO Brick Finder/Pods-LEGO Brick Finder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LEGO_Brick_Finder : NSObject
+@end
+@implementation PodsDummy_Pods_LEGO_Brick_Finder
+@end
---
+++ LEGO Brick Finder/Pods/Target Support Files/Pods-LEGO Brick FinderTests/Pods-LEGO Brick FinderTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LEGO_Brick_FinderTests : NSObject
+@end
+@implementation PodsDummy_Pods_LEGO_Brick_FinderTests
+@end
